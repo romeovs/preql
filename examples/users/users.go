cfb6ed069109cd835387ea7a620c031977167d6c
Romeo <romeovs@gmail.com>
2019-09-04 15:58:58 +0200

    Add example users package

---
+++ examples/users/users.go
@@ -0,0 +1,31 @@
+package users
+
+import (
+	"context"
+
+	"github.com/romeovs/preql/sql"
+)
+
+// A User in the database.
+type User struct {
+	// Username is the user's username.
+	Username string `sql:"username"`
+
+	// Email is the user's email address, normalized.
+	Email string `sql:"email"`
+}
+
+// A Repository that holds users.
+type Repository struct {
+	sql sql.Interface
+}
+
+// FindUserByUsername finds a user with the given username.
+//
+// sql:
+//   SELECT * FROM users
+//     WHERE lower(username) = lower(:username)
+//     LIMIT 1
+func (r Repository) FindUserByUsername(ctx context.Context, username string) (*User, error) {
+	return nil, nil
+}
